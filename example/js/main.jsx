ae595498776d9ce6c74dc5d073ea848b2778b5e7
Justin McCandless <justinjmccandless@gmail.com>
2017-11-27 22:11:56 -0800

    Example uses react-audio-player via npm

--- example/js/main.jsx
+++ example/js/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactAudioPlayer from '../../src/index';
+import ReactAudioPlayer from 'react-audio-player';
 
 ReactDOM.render(
   <ReactAudioPlayer
